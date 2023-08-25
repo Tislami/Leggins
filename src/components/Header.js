e64b71ac03383d786178bc83f8e939be8f45cee9
Tariyel <islami.tariyel@gmail.com>
2023-08-25 08:14:21 -0400

    axios added

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import AppLogo from "./AppLogo";
 
 const Header = () => { 
     return (
-        <header className="w-full h-20 bg-white border-b-[1px] border-b-gray-800 font-titleFont">
+        <header className="w-full h-20 bg-white border-b-[1px] border-b-gray-800 font-titleFont sticky top-0 z-50">
             <div className="max-w-screen-xl h-full mx-auto flex items-center justify-between">
                 <AppLogo/>
                 <div className="flex items-center gap-8"> 
